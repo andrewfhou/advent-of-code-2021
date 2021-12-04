03120d7024dfd24a4404c935765e9c6b1461df25
Andrew Hou <andrewfhou@gmail.com>
2021-12-04 00:34:40 -0500

    day 4 solved

--- day04/day04.py
+++ day04/day04.py
@@ -8,15 +8,54 @@ print('+-------------------------+')
 
 START_READ = CURR_MS()
 print('\nREADING FILE... ',end='')
-with open("input.txt") as file:
-    inputs = file.read().strip().split()
+file = open('input.txt')
+nums = [int(x) for x in file.readline().strip().split(",")]
+boards = [
+    [[int(i) for i in l.split()] for l in x.splitlines()]
+    for x in file.read().strip().split("\n\n")
+]
 print('%.6fms\n' % (CURR_MS() - START_READ))
 
 def part_one():
-    return 0
+    for n in nums:
+        for b in boards:
+            for i, row in enumerate(b):
+                for j, cell in enumerate(row):
+                    if cell == n:
+                        b[i][j] = None
+            for row in b:
+                if all(x is None for x in row):
+                    score = sum(i for r in b for i in r if i is not None)
+                    return score * n
+            for col in zip(*b):
+                if all(x is None for x in col):
+                    score = sum(i for r in b for i in r if i is not None)
+                    return score * n
 
 def part_two():
-    return 0
+    wins = []
+    win = None
+
+    for n in nums:
+        if sum(x not in wins for x in boards) == 1:
+            win = next(i for i, x in enumerate(boards) if x not in wins)
+        for bdx, b in enumerate(boards):
+            for i, row in enumerate(b):
+                for j, cell in enumerate(row):
+                    if cell == n:
+                        b[i][j] = None
+            for row in b:
+                if all(x is None for x in row):
+                    if bdx == win:
+                        score = sum(i for r in b for i in r if i is not None)
+                        return score * n
+                    wins.append(b)
+            for col in zip(*b):
+                if all(x is None for x in col):
+                    if bdx == win:
+                        score = sum(i for r in b for i in r if i is not None)
+                        return score * n
+                    wins.append(b)
 
 START_ONE = CURR_MS()
 print('PART ONE: ' + str(part_one()))
