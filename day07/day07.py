3051d3c4bb837ea737884586bef06774b18155c8
Andrew Hou <andrewfhou@gmail.com>
2021-12-07 00:09:02 -0500

    day 7 solved

--- day07/day07.py
+++ day07/day07.py
@@ -9,14 +9,28 @@ print('+-------------------------+')
 START_READ = CURR_MS()
 print('\nREADING FILE... ',end='')
 with open("input.txt") as file:
-    inputs = file.read().strip().split()
+    inputs = [int(x) for x in file.read().strip().split(',')]
 print('%.6fms\n' % (CURR_MS() - START_READ))
 
 def part_one():
-    return 0
+    minx = min(inputs)
+    maxx = max(inputs)
+    best = 10000000000000000000
+    for i in range(minx, maxx):
+        cost = sum(abs(i - j) for j in inputs)
+        best = min(cost, best)
+    return best
 
+def f(n):
+    return (n * (n + 1)) // 2
 def part_two():
-    return 0
+    minx = min(inputs)
+    maxx = max(inputs)
+    best = 10000000000000000000
+    for i in range(minx, maxx):
+        cost = sum(f(abs(i - j)) for j in inputs)
+        best = min(cost, best)
+    return best
 
 START_ONE = CURR_MS()
 print('PART ONE: ' + str(part_one()))
