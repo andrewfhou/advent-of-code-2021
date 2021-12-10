8af87055709e7713b1bafe5b5f16f4239f104a3b
Andrew Hou <andrewfhou@gmail.com>
2021-12-10 00:23:39 -0500

    day10 solved

--- day10/day10.py
+++ day10/day10.py
@@ -9,14 +9,59 @@ print('+-------------------------+')
 START_READ = CURR_MS()
 print('\nREADING FILE... ',end='')
 with open("input.txt") as file:
-    inputs = file.read().strip().split()
+    inputs = file.read().splitlines()
 print('%.6fms\n' % (CURR_MS() - START_READ))
 
+corr_scores = { ')' : 3,
+           ']' : 57,
+           '}' : 1197,
+           '>' : 25137 }
+
+matches = { '(' : ')',
+            '[' : ']',
+            '{' : '}',
+            '<' : '>' }
+
+incomplete = list()
+
 def part_one():
-    return 0
+    score = 0
+    for line in inputs:
+        corrupt = False
+        stack = list()
+        for c in line:
+            if c in ('(', '[', '{', '<'):
+                stack.append(c)
+            else:
+                x = stack.pop()
+                if matches[x] != c:
+                    corrupt = True
+                    score += corr_scores[c]
+        if not corrupt:
+            incomplete.append(line)
+    return score
+
+inc_scores = { '(' : 1,
+               '[' : 2,
+               '{' : 3,
+               '<' : 4 }
 
 def part_two():
-    return 0
+    scores = list()
+    for line in incomplete:
+        score = 0
+        stack = list()
+        for c in line:
+            if c in ('(', '[', '{', '<'):
+                stack.append(c)
+            else:
+                x = stack.pop()
+        while len(stack) > 0:
+            c = stack.pop()
+            score *= 5
+            score += inc_scores[c]
+        scores.append(score)
+    return sorted(scores)[(int) ((len(scores) - 1) / 2) ]
 
 START_ONE = CURR_MS()
 print('PART ONE: ' + str(part_one()))
