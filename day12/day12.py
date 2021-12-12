ba699cfd1921b8e669ad12aa31a8bce159780dd0
Andrew Hou <andrewfhou@gmail.com>
2021-12-12 00:39:02 -0500

    day12 solved

--- day12/day12.py
+++ day12/day12.py
@@ -1,4 +1,5 @@
 import time
+from collections import defaultdict, Counter
 
 CURR_MS = lambda: time.time() * 1000
 
@@ -8,15 +9,32 @@ print('+-------------------------+')
 
 START_READ = CURR_MS()
 print('\nREADING FILE... ',end='')
-with open("test.txt") as file:
-    inputs = file.read().strip().split()
+with open("input.txt") as file:
+    inputs = file.read().strip().split('\n')
+    edges = defaultdict(list)
+    for line in inputs:
+        s, t = line.split('-')
+        edges[s].append(t)
+        edges[t].append(s)
 print('%.6fms\n' % (CURR_MS() - START_READ))
 
+def solve(edges, curr, prev, p2):
+    if curr.islower():
+        prev = prev + [curr]
+    if curr == 'end':
+        return 1
+    total = 0
+    for adj in edges[curr]:
+        if (adj not in prev) or \
+           (p2 and max(Counter(prev).values()) == 1 and adj != 'start'):
+            total += solve(edges, adj, prev, p2)
+    return total
+
 def part_one():
-    return 0
+    return solve(edges, 'start', [], False)
 
 def part_two():
-    return 0
+    return solve(edges, 'start', [], True)
 
 START_ONE = CURR_MS()
 print('PART ONE: ' + str(part_one()))
